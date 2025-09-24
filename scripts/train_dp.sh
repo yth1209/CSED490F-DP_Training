3f272db1d701680ed84ec807211508d521007e1a
taehyun.yoo <yth001209@gmail.com>
2025-09-24 17:43:10 +0900

    add nsys cmd

--- scripts/train_dp.sh
+++ scripts/train_dp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python train_cifar.py --data dataset/cifar10 --ckpt runs/default/${TIMESTAMP}
-CUDA_VISIBLE_DEVICES=0,1 python train_cifar.py --dp --data dataset/cifar10 --ckpt runs/dp_2/${TIMESTAMP}
+NSYS_CMD="nsys profile --force-overwrite true -o nsight_logs/1_gpu_${TIMESTAMP}.nsys-rep"
+CUDA_VISIBLE_DEVICES=0 ${NSYS_CMD} python train_cifar.py --data dataset/cifar10 --ckpt runs/default/${TIMESTAMP}
+
+NSYS_CMD="nsys profile --force-overwrite true -o nsight_logs/2_gpu_${TIMESTAMP}.nsys-rep"
+CUDA_VISIBLE_DEVICES=0,1 ${NSYS_CMD} python train_cifar.py --dp --data dataset/cifar10 --ckpt runs/dp_2/${TIMESTAMP}
